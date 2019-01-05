b23ebb96b02427d20b7e97362b7de9c3f75812a8
raymas <cSYvL6@yandex.com>
2019-01-05 14:38:55 +0100

    Adding arguments parser

--- vigenere.lua
+++ vigenere.lua
@@ -3,6 +3,9 @@
 --
 -- decryptor for vigenere method
 
+-- paramters
+local params = {...}
+
 -- LANGUAGE FREQUENCIES
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 english = "eariotnslcudpmhgbfywkvxzjq"
@@ -142,34 +145,176 @@ function onePadding(int)
   return (int > 0) and int or 1
 end
 
+function isInList(key, list)
+  for _,v in pairs(list) do
+    if v == key then
+      return true
+    end
+  end
+  return false
+end
 
--- MAIN FUNCTION
+function getIndex(key, list)
+  for t,v in pairs(list) do
+    if v == key then
+      return t
+    end
+  end
+  return nil
+end
 
-longText = [===[
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed ipsum sit amet metus suscipit pharetra. Pellentesque imperdiet a enim ut cursus. Nullam euismod blandit urna, sit amet faucibus lectus tristique ac. Nulla congue ac lacus nec tempus. Suspendisse dapibus metus vel diam porta interdum. Donec pulvinar a magna id dignissim. Nulla eleifend laoreet urna.
+-- Help
+function printHelp()
+  print("Usage : lua vigenere.lua [OPTIONS]")
+  print("")
+  print("-e\tencrypt by entering a key, and a text")
+  print("-d\tdecrypt by entering a key, and a text")
+  print("-c\tcrack")
+end
 
-Aliquam sit amet risus vitae neque ornare sodales nec vel odio. Vivamus eu est id lacus faucibus eleifend. Nulla nec pharetra massa. Duis quis iaculis elit. Cras et hendrerit elit. Sed at neque et orci semper eleifend vitae eget nisl. Morbi malesuada pellentesque urna, sit amet efficitur purus eleifend accumsan. Etiam lectus nisl, consectetur eget lacus vitae, cursus facilisis libero. Nullam vitae vehicula eros. Vestibulum vitae commodo velit. Integer elementum pellentesque venenatis. Duis eget urna ac lectus ullamcorper ullamcorper et vel risus. In ante est, consequat eget lacinia vel, tincidunt nec erat. Integer lobortis mi ante, ut condimentum purus commodo vel. In hac habitasse platea dictumst. Suspendisse vulputate hendrerit nisi elementum vulputate.
+-- TEST FUNCTION
+function TEST()
+  longText = [===[
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed ipsum sit amet metus suscipit pharetra. Pellentesque imperdiet a enim ut cursus. Nullam euismod blandit urna, sit amet faucibus lectus tristique ac. Nulla congue ac lacus nec tempus. Suspendisse dapibus metus vel diam porta interdum. Donec pulvinar a magna id dignissim. Nulla eleifend laoreet urna. Aliquam sit amet risus vitae neque ornare sodales nec vel odio. Vivamus eu est id lacus faucibus eleifend. Nulla nec pharetra massa. Duis quis iaculis elit. Cras et hendrerit elit. Sed at neque et orci semper eleifend vitae eget nisl. Morbi malesuada pellentesque urna, sit amet efficitur purus eleifend accumsan. Etiam lectus nisl, consectetur eget lacus vitae, cursus facilisis libero. Nullam vitae vehicula eros. Vestibulum vitae commodo velit. Integer elementum pellentesque venenatis. Duis eget urna ac lectus ullamcorper ullamcorper et vel risus. In ante est, consequat eget lacinia vel, tincidunt nec erat. Integer lobortis mi ante, ut condimentum purus commodo vel. In hac habitasse platea dictumst. Suspendisse vulputate hendrerit nisi elementum vulputate.
+  Nulla posuere vestibulum sem, id pulvinar quam malesuada dapibus. Integer porttitor ac velit quis malesuada. Cras eu ligula quis arcu dictum ultrices ac at odio. Nulla sit amet nulla sed odio vehicula aliquam sed nec tortor. Sed viverra enim sit amet odio elementum, ut vehicula erat convallis. Ut leo mauris, blandit ut pulvinar sit amet, tristique at quam. Integer est eros, ornare non erat at, vulputate tempus neque. Sed ornare felis sit amet lectus pellentesque, ac suscipit ex venenatis. In finibus vel lorem ac dignissim.
+  Aenean rutrum sodales urna, non venenatis diam scelerisque et. Aliquam porttitor augue sed dapibus auctor. Nulla tempus leo nec leo tristique interdum. Maecenas vehicula, enim ac efficitur ullamcorper, ligula ante elementum ligula, vitae semper enim sapien sit amet felis. Nullam lobortis accumsan est, accumsan lobortis neque sollicitudin eget. Morbi lacus arcu, vulputate sed dictum sed, auctor id elit. Proin nisl dui, pellentesque et diam et, mattis convallis sapien. Aliquam semper imperdiet ante, non suscipit risus molestie sed. Morbi gravida sem metus, sed varius augue eleifend sed. Donec pulvinar ipsum nunc, at mattis ex sodales at. In dignissim libero nisl, sit amet pharetra diam aliquet non. Aliquam consectetur lacinia ex ac sollicitudin.
 
-Nulla posuere vestibulum sem, id pulvinar quam malesuada dapibus. Integer porttitor ac velit quis malesuada. Cras eu ligula quis arcu dictum ultrices ac at odio. Nulla sit amet nulla sed odio vehicula aliquam sed nec tortor. Sed viverra enim sit amet odio elementum, ut vehicula erat convallis. Ut leo mauris, blandit ut pulvinar sit amet, tristique at quam. Integer est eros, ornare non erat at, vulputate tempus neque. Sed ornare felis sit amet lectus pellentesque, ac suscipit ex venenatis. In finibus vel lorem ac dignissim.
+  In non auctor leo. Praesent maximus luctus tortor. Donec luctus, dolor vel mattis porttitor, orci felis vestibulum eros, sit amet tempus lorem felis ut nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc non consequat neque. Donec mollis gravida urna, sed iaculis augue pellentesque non. Suspendisse ac porta urna. Sed congue finibus felis viverra varius. In nisl justo, iaculis sed fringilla nec, semper non dui. Vivamus cursus ante eu interdum ultricies. Nunc volutpat consectetur volutpat. Nam ut massa nec massa eleifend mollis non sed ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;]===]
+  print("#----------------------------#")
+  r = encrypt(longText, "lua", english)
+  print(r)
+  print("#----------------------------#")
+  print("        DECRYPTION            ")
+  print("#----------------------------#")
+  len = keylengthguess(r, english, 5)
+  print("Length guess is : " .. len.keylength)
+  pseudoKey = guessingKey(r, english, len.keylength)
+  print("Psuedo key guess is : ".. pseudoKey)
+  print("#----------------------------#")
+  print("         TESTING              ")
+  print("#----------------------------#")
+  d = decrypt(r, pseudoKey, english)
+  print(d)
+  print("#----------------------------#")
+end
 
-Aenean rutrum sodales urna, non venenatis diam scelerisque et. Aliquam porttitor augue sed dapibus auctor. Nulla tempus leo nec leo tristique interdum. Maecenas vehicula, enim ac efficitur ullamcorper, ligula ante elementum ligula, vitae semper enim sapien sit amet felis. Nullam lobortis accumsan est, accumsan lobortis neque sollicitudin eget. Morbi lacus arcu, vulputate sed dictum sed, auctor id elit. Proin nisl dui, pellentesque et diam et, mattis convallis sapien. Aliquam semper imperdiet ante, non suscipit risus molestie sed. Morbi gravida sem metus, sed varius augue eleifend sed. Donec pulvinar ipsum nunc, at mattis ex sodales at. In dignissim libero nisl, sit amet pharetra diam aliquet non. Aliquam consectetur lacinia ex ac sollicitudin.
 
-In non auctor leo. Praesent maximus luctus tortor. Donec luctus, dolor vel mattis porttitor, orci felis vestibulum eros, sit amet tempus lorem felis ut nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc non consequat neque. Donec mollis gravida urna, sed iaculis augue pellentesque non. Suspendisse ac porta urna. Sed congue finibus felis viverra varius. In nisl justo, iaculis sed fringilla nec, semper non dui. Vivamus cursus ante eu interdum ultricies. Nunc volutpat consectetur volutpat. Nam ut massa nec massa eleifend mollis non sed ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; ]===]
+-- Main Function
+function main(args)
+  if #args > 0 then
+    if isInList("e", args) then
+      cmdEncode(args)
+    elseif isInList("d", args) then
+      cmdDecode(args)
+    elseif isInList("c", args) then
+      cmdCrack(args)
+    elseif isInList("-h", args) or isInList("--help", args) then
+      printHelp()
+    else
+      print("No command found. Use --help for list and usages")
+    end
+  else
+    printHelp()
+  end
+end
 
-print("#----------------------------#")
-r = encrypt(longText, "lua", english)
-print(r)
-print("#----------------------------#")
-print("        DECRYPTION            ")
-print("#----------------------------#")
-len = keylengthguess(r, english, 5)
-print("Length guess is : " .. len.keylength)
-pseudoKey = guessingKey(r, english, len.keylength)
-print("Psuedo key guess is : ".. pseudoKey)
-print("#----------------------------#")
-print("         TESTING              ")
-print("#----------------------------#")
-d = decrypt(r, pseudoKey, english)
-print(d)
-print("#----------------------------#")
+function cmdEncode(cli)
+  local string = nil
+  local key = nil
+  local language = english
+
+  if isInList("--file", cli) then
+    local path = getIndex("--file", cli) + 1
+    if cli[path] ~= nil then
+      file = io.open(cli[path], "r")
+      string = file:read("*a")
+    else
+      print("Error argument : incorrect path")
+      os.exit()
+    end
+  elseif isInList("--raw", cli) then
+    local text = getIndex("--raw", cli) + 1
+    if cli[text] ~= nil then
+      string = cli[text]
+    else
+      print("Error argument : incorrect text")
+      os.exit()
+    end
+  end
+  key = cli[#cli]
+  print(encrypt(string, key, english))
+end
+
+function cmdDecode(cli)
+  local string = nil
+  local key = nil
+  local language = english
+
+  if isInList("--file", cli) then
+    local path = getIndex("--file", cli) + 1
+    if cli[path] ~= nil then
+      file = io.open(cli[path], "r")
+      string = file:read("*a")
+    else
+      print("Error argument : incorrect path")
+      os.exit()
+    end
+  elseif isInList("--raw", cli) then
+    local text = getIndex("--raw", cli) + 1
+    if cli[text] ~= nil then
+      string = cli[text]
+    else
+      print("Error argument : incorrect text")
+      os.exit()
+    end
+  end
+  key = cli[#cli]
+  print(decrypt(string, key, language))
+end
+
+
+function cmdCrack(cli)
+  local string = nil
+  local keylength = nil
+  local language = english
+
+  if isInList("--file", cli) then
+    local path = getIndex("--file", cli) + 1
+    if cli[path] ~= nil then
+      file = io.open(cli[path], "r")
+      string = file:read("*a")
+    else
+      print("Error argument : incorrect path")
+      os.exit()
+    end
+  elseif isInList("--raw", cli) then
+    local text = getIndex("--raw", cli) + 1
+    if cli[text] ~= nil then
+      string = cli[text]
+    else
+      print("Error argument : incorrect text")
+      os.exit()
+    end
+  end
+
+  if isInList("--maxkeylength", cli) then
+    local mkl = getIndex("--maxkeylength", cli) + 1
+    if cli[mkl] ~= nil then
+      keylength = cli[mkl]
+    end
+  end
+
+  if keylength then
+    local keyguess = guessingKey(string, language, keylength)
+    print(decrypt(string, keyguess, language))
+  else
+    local len = keylengthguess(string, english, 5)
+    print("Length guess is : " .. len.keylength)
+    local pseudoKey = guessingKey(string, english, len.keylength)
+    print("Psuedo key guess is : ".. pseudoKey)
+    print(decrypt(string, pseudoKey, language))
+  end
+end
+
+main(params)
